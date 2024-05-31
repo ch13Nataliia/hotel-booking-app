20f2e6fd53c52cfdefadf747a645edbbebcaebc7
Nataliia Tsirul <tcirulnatalya@gmail.com>
2024-05-31 14:00:03 +0100

    added header

---
+++ src/components/Header.tsx
@@ -0,0 +1,21 @@
+import { Link } from 'react-router-dom';
+
+const Header = () => {
+  return (
+    <div className="bg-green-800 py-6">
+      <div className="container mx-auto flex justify-between">
+        <span className="text-3xl text-white font-bold tracking-tight">
+          <Link to="/">JHolidays.com</Link>
+        </span>
+        <span className="flex space-x-2">
+          <Link
+            to="/sign-in"
+            className="flex items-center bg-white rounded-md text-green-600 px-3 font-bold hover:bg-gray-100 hover:text-blue-500"
+          >Sign-in</Link>
+        </span>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
